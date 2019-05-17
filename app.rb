3aece82557aeaf8a0023b4a87d730f2242e9bc84
lieby weintraub <liebahinda@gmail.com>
2019-05-17 02:42:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get "/name" do 
-     "My name is   "
-   erb :'name/index.html.erb'
+     "My name is Lieby 
    end 
   
      
