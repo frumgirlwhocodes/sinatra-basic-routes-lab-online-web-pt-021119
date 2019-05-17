a8bf826e9ee70a032e84f68d05cccdea00a30291
lieby weintraub <liebahinda@gmail.com>
2019-05-17 02:43:44 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,7 +5,15 @@ class App < Sinatra::Base
   get "/name" do 
      "My name is Lieby" 
    end 
+   get "/hometown" do 
+     "My hometown is Brooklyn" 
+   end 
   
+get "/favorite-song" do 
+     "My n" 
+   end 
+  
+
      
   
 end
